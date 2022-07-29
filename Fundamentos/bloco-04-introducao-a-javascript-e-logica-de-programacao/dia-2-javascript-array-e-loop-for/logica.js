2706f488fdf24e44b5766df8783be616d9024c2e
Ricardo Torres Teixeira Junior <r.torresteixeirajr@gmail.com>
2022-07-29 09:34:51 -0300

    Resolvendo exercios de fixação

---
+++ Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/logica.js
@@ -0,0 +1,46 @@
+// exercicio 1
+
+let soma = 0;
+
+for (let index 0; index <= 50; index += 1); {
+    soma += i;
+
+}
+
+console.log("A soma de 1 a 50 é: "  , soma");
+
+
+// exercicio 2
+
+let qnt = 0;
+for (i=2; i<=150; i+1) {
+    if (i % 3 == 0) {
+        qnt +1;
+    }
+}
+if (qnt == 50){
+    console.log('Bingo');
+}
+
+
+
+
+
+
+
+
+
+
+
+// exercicio 3
+
+let pessoaX = 20;
+let pessoaY = 15;
+let pessoaZ = 29;
+
+if (idade <=18) {
+    
+}
+else (idade >=30) {
+    
+}
