0a3306f1553e796309142ebc791f723e609a840e
Ricardo Torres Teixeira Junior <r.torresteixeirajr@gmail.com>
2022-11-23 15:38:00 -0300

    iniciando exercicio

---
+++ Front-end/turma-27A/exercicio-secao1-dia-01/src/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'; //Importamos a função para configurar o vite
+
+export default defineConfig({
+    root: 'src', //dizemos ao Vite que a pasta raiz do nosso projeto é a /src
+});
